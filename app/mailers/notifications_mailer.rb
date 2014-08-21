617d04578f6f56fa86709d0722325e3dcaf7a423
100lp <spb.gurme@gmail.com>
2014-08-21 19:36:27 +0400

    HOPE

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,4 +1,6 @@
 class NotificationsMailer < ActionMailer::Base
+  default from: "message@art-povar.ru"
+
   def new_message(message)
   	@message = message
   	mail(to: 'nadezhda_ilina@art-povar.ru')
